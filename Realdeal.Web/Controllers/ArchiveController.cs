f302092f92bf4d9ecbd5b32f1f3ceb893af54e4b
vesko662 <vesko662@abv.bg>
2021-08-05 18:05:17 +0300

    Added archive contoller and the admin cannot archive the user advert.

---
+++ Realdeal.Web/Controllers/ArchiveController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Realdeal.Web.Controllers
+{
+    [Authorize]
+    public class ArchiveController : Controller
+    {
+        public IActionResult Add(string advertId)
+        {
+
+            return RedirectToAction(nameof(All));
+        }
+
+        public IActionResult All()
+        {
+            return View();
+        }
+        public IActionResult Upload(string advertId)
+        {
+            return RedirectToAction(nameof(AdvertController),nameof(AdvertController.All));
+        }
+    }
+}
